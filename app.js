885c280bc1af4483af5658256738044bec9866e8
MandhareAmit <73575527+MandhareAmit@users.noreply.github.com>
2021-08-05 09:31:40 +0530

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//importing express and other deppendencies and routes
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -8,16 +9,18 @@ const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+// view path setting
 app.set('views', path.join(__dirname, 'views'))
 app.use(methodOverride('_method'))
 
 app.use(express.static(path.join(__dirname, './views')));
 app.set('view engine', 'ejs');
 
+// routes
 app.use('/home', route);
 
 
-
+// server listning 
 const port = process.env.port || 3000;
 
 app.listen(port, () => {
